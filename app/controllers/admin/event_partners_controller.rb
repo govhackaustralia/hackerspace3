1fed8972be2bcb790acfc2ccfa96c8b1b107e775
Luke Cassar <lukecassar7@gmail.com>
2018-07-28 20:48:28 +1000

    Fix event partners controller styling 'error'

--- app/controllers/admin/event_partners_controller.rb
+++ app/controllers/admin/event_partners_controller.rb
@@ -3,19 +3,16 @@ class Admin::EventPartnersController < ApplicationController
   before_action :check_for_privileges
 
   def new
-    @event = Event.find(params[:event_id])
-    @event_partner = EventPartner.new
+    new_event_partner
     return if params[:term].nil? || params[:term] == ''
     @sponsor = Sponsor.find_by_name(params[:term])
     @sponsors = Sponsor.search(params[:term]) unless @sponsor.present?
   end
 
   def create
-    @event = Event.find(params[:event_id])
-    @sponsor = Sponsor.find(params[:sponsor_id])
-    @event_partner = EventPartner.new(event: @event, sponsor: @sponsor)
+    create_new_event_partner
     if @event_partner.save
-      flash[:notice] = "New Event Partner Added."
+      flash[:notice] = 'New Event Partner Added.'
       redirect_to admin_region_event_path(@event.region, @event)
     else
       flash.now[:notice] = @assignment.errors.full_messages.to_sentence
@@ -29,5 +26,16 @@ class Admin::EventPartnersController < ApplicationController
     return if current_user.admin_privileges?
     flash[:error] = 'You must have valid assignments to access this section.'
     redirect_to root_path
+  end
+
+  def new_event_partner
+    @event = Event.find(params[:event_id])
+    @event_partner = EventPartner.new
+  end
+
+  def create_new_event_partner
+    @event = Event.find(params[:event_id])
+    @sponsor = Sponsor.find(params[:sponsor_id])
+    @event_partner = EventPartner.new(event: @event, sponsor: @sponsor)
   end
 end
