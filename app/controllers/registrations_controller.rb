af98c16eda3eb261c3671bf550ad8cfaa8fc191e
Luke Cassar <lukecassar7@gmail.com>
2018-07-31 23:04:07 +1000

    Redo event registration process.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @region = @event.region
     @registration = @event.registrations.new
+    @user = current_user
   end
 
   def show
@@ -19,12 +20,12 @@ class RegistrationsController < ApplicationController
     @event = Event.find(params[:event_id])
     @region = @event.region
     @registration = Registration.find(params[:id])
+    @user = current_user
   end
 
   def update
-    @event = Event.find(params[:event_id])
-    @registration = Registration.find(params[:id])
-    @registration.update(registration_params)
+    update_registration
+    update_user_preferences
     if @registration.save
       flash[:notice] = 'Your registration has been updated'
       redirect_to event_registration_path(@event, @registration)
@@ -35,13 +36,10 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @event = Event.find(params[:event_id])
-    @region = @event.region
-    @registration = @event.registrations.new(registration_params)
-    @registration.update(assignment: current_user.event_assignment)
+    create_new_registration
+    update_user_preferences
     if @registration.save
-      flash[:notice] = 'You have registered for this event.'
-      redirect_to event_registration_path(@event, @registration)
+      handle_new_save
     else
       flash[:notice] = @registration.errors.full_messages.to_sentence
       render 'new'
@@ -52,5 +50,34 @@ class RegistrationsController < ApplicationController
 
   def registration_params
     params.require(:registration).permit(:status)
+  end
+
+  def handle_new_save
+    flash[:notice] = if params[:status] == ATTENDING
+                       'You have registered for this event.'
+                     else
+                       'You have been added to the waitlist.'
+                     end
+    redirect_to event_registration_path(@event, @registration)
+  end
+
+  def create_new_registration
+    @event = Event.find(params[:event_id])
+    @registration = @event.registrations.new(status: params[:status])
+    @user = current_user
+    @registration.update(assignment: @user.event_assignment)
+  end
+
+  def update_registration
+    @event = Event.find(params[:event_id])
+    @registration = Registration.find(params[:id])
+    @registration.update(registration_params)
+    @user = current_user
+  end
+
+  def update_user_preferences
+    @user.update(preferred_name: params[:preferred_name])
+    @user.update(organisation_name: params[:organisation_name])
+    @user.update(dietary_requirements: params[:dietary_requirements])
   end
 end
