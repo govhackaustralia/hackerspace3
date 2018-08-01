65046d6cdc9aebf63a58b169ba49d4f2cf953cf6
Luke Cassar <lukecassar7@gmail.com>
2018-08-01 17:29:28 +1000

    Move User Devise views to own folder, Add mail previews.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ApplicationMailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  config.scoped_views = true
+  # config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
---
+++ test/mailers/previews/devise/devise_mailer_preview.rb
@@ -0,0 +1,23 @@
+# Preview all emails at http://localhost:3000/rails/mailers/devise_mailer_preview.rb
+class Devise::MailerPreview < ActionMailer::Preview
+
+  def confirmation_instructions
+    Devise::Mailer.confirmation_instructions(User.first, "faketoken")
+  end
+
+  def email_changed
+    Devise::Mailer.email_changed(User.first, {})
+  end
+
+  def password_change
+    Devise::Mailer.password_change(User.first, {})
+  end
+
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.first, "faketoken")
+  end
+
+  def unlock_instructions
+    Devise::Mailer.unlock_instructions(User.first, "faketoken")
+  end
+end
