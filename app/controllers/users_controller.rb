eb67849ff2b9c9686be2f82ebaf5ec60635b735e
Luke Cassar <lukecassar7@gmail.com>
2018-08-02 22:25:12 +1000

    Phone number added to approved editable attributes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:full_name, :preferred_name, :preferred_img,
-                                 :tshirt_size, :twitter, :mailing_list, :challenge_sponsor_contact_place,
+                                 :tshirt_size, :twitter, :phone_number, :mailing_list, :challenge_sponsor_contact_place,
                                  :challenge_sponsor_contact_enter, :my_project_sponsor_contact,
                                  :me_govhack_contact, :dietary_requirements, :organisation_name,
                                  :how_did_you_hear, :govhack_img, :accepted_terms_and_conditions)
