a9b274db6d4f7d4cad5974065afb8901cc9d4317
Luke Cassar <lukecassar7@gmail.com>
2018-08-03 07:52:37 +1000

    Remove last development migration files before production.

--- db/migrate/20180801121708_event_type.rb
+++
@@ -1,5 +0,0 @@
-class EventType < ActiveRecord::Migration[5.2]
-  def change
-    add_column :events, :type, :string
-  end
-end
--- db/migrate/20180801130707_events_remove.rb
+++
@@ -1,5 +0,0 @@
-class EventsRemove < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :events, :category_type, :string
-  end
-end
