6478acd6c60bbfbc33de4d38fa7ba15a72266a03
Luke Cassar <lukecassar7@gmail.com>
2018-08-26 15:24:47 +1000

    Add Data Set search and show view.

--- app/controllers/data_sets_controller.rb
+++ app/controllers/data_sets_controller.rb
@@ -3,4 +3,8 @@ class DataSetsController < ApplicationController
     @competition = Competition.current
     @data_sets = @competition.data_sets
   end
+
+  def show
+    @data_set = DataSet.find(params[:id])
+  end
 end
--- app/helpers/data_sets_helper.rb
+++ app/helpers/data_sets_helper.rb
@@ -1,2 +1,11 @@
 module DataSetsHelper
+  def filter_data_sets(data_sets, term)
+    return data_sets if term.nil?
+    filtered_data_sets = []
+    @data_sets.each do |data_set|
+      data_set_string = "#{data_set.name} #{data_set.description} #{data_set.region.name}".downcase
+      filtered_data_sets << data_set if data_set_string.include? term.downcase
+    end
+    filtered_data_sets
+  end
 end
