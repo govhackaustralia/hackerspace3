cf8c55425fa016c421e9af50646f310494a200a1
Luke Cassar <lukecassar7@gmail.com>
2018-08-27 21:53:35 +1000

    Add block team edit on competition end.

--- app/helpers/team_management/teams_helper.rb
+++ app/helpers/team_management/teams_helper.rb
@@ -1,2 +1,6 @@
 module TeamManagement::TeamsHelper
+  def competition_closed?
+    team_time = Time.now.in_time_zone(@team.time_zone).to_formatted_s(:number)
+    @competition.end_time.to_formatted_s(:number) < team_time
+  end
 end
