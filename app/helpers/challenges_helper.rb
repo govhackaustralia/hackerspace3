604d4964e9747ff896472dbf4a3d8f1d232b718c
Luke Cassar <lukecassar7@gmail.com>
2018-08-29 21:52:59 +1000

    Add entry count on public challenge screen.

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,2 +1,8 @@
 module ChallengesHelper
+  def challenge_entry_counts
+    entries = Entry.where(challenge_id: @challenges.pluck(:id))
+    @challenge_id_to_entry_count = {}
+    @challenges.each { |challenge| @challenge_id_to_entry_count[challenge.id] = 0 }
+    entries.each { |entry| @challenge_id_to_entry_count[entry.challenge_id] += 1 }
+  end
 end
