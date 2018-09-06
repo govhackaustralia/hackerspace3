f55a9291f8525d3209f237f36e365ae9ce2710a4
Luke Cassar <lukecassar7@gmail.com>
2018-09-06 22:25:17 +1000

    Add id_team_project object to events teams to make less db calls.

--- app/controllers/events/teams_controller.rb
+++ app/controllers/events/teams_controller.rb
@@ -2,5 +2,6 @@ class Events::TeamsController < ApplicationController
   def index
     @event = Event.find_by(identifier: params[:event_identifier])
     @teams = @event.teams.where(published: true)
+    @id_teams_projects = Team.id_teams_projects(@teams.pluck(:id))
   end
 end
