baef75d9046d874cd12c093a2d603affe15ca095
Luke Cassar <lukecassar7@gmail.com>
2018-09-08 16:28:07 +1000

    Add regional challenge entry tallies to public entries page.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,5 +5,6 @@ class EntriesController < ApplicationController
     @region_counts = Region.national_challenges_region_counts(passed_checkpoint_ids)
     @region_names = Region.where.not(parent_id: nil).order(:name).pluck(:name)
     @challenge_names = Region.root.challenges.order(:name).pluck(:name)
+    @sub_regions = Region.where.not(parent_id: nil).order(:name)
   end
 end
