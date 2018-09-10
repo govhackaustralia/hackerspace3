d61ea2ffd0fec1bc1e212cfd4acfad14bc9cf290
Luke Cassar <lukecassar7@gmail.com>
2018-09-10 22:35:09 +1000

    Add JS pop in new window for scorecards.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var windowObjectReference;
+var strWindowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+
+function openRequestedPopup() {
+  windowObjectReference = window.open(_scorecard_path, _scorecard_title, strWindowFeatures);
+}
+
+$('#open-scorecard').click(function (event) {
+  openRequestedPopup();
+  event.preventDefault();
+});
