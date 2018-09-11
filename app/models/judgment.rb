24abd160cdbd6ec6ba2bfa674503f02563a6b055
Luke Cassar <lukecassar7@gmail.com>
2018-09-12 00:53:16 +1000

    Add validation to prevent duplicate judgments being created.

--- app/models/judgment.rb
+++ app/models/judgment.rb
@@ -1,4 +1,7 @@
 class Judgment < ApplicationRecord
   belongs_to :scorecard
   belongs_to :criterion
+
+  validates :scorecard_id, uniqueness: { scope: :criterion_id,
+                                          message: 'Judgment already exists.' }
 end
