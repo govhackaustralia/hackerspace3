e6f88e0b6286ba884c93d7538b3e4c53c4f5739d
Luke Cassar <lukecassar7@gmail.com>
2018-10-01 21:58:29 +1000

    add csv download for data sets with project names that use data set.

--- app/models/data_set.rb
+++ app/models/data_set.rb
@@ -13,13 +13,31 @@ class DataSet < ApplicationRecord
     results
   end
 
+  def self.data_set_project_helper
+    data_set_urls = all.pluck(:url)
+    team_data_sets = TeamDataSet.where(url: data_set_urls)
+    team_ids = team_data_sets.pluck(:team_id)
+    id_teams_projects = Team.id_teams_projects(team_ids)
+    url_to_project_names = {}
+    data_set_urls.each {|url| url_to_project_names[url] = [] }
+    team_data_sets.each do |team_data_set|
+      project = id_teams_projects[team_data_set.team_id][:current_project]
+      url_to_project_names[team_data_set.url] << project.project_name
+    end
+    url_to_project_names
+  end
+
   require 'csv'
 
   def self.to_csv(options = {})
+    data_set_columns = %w[name url description]
+    combined = data_set_columns + ['projects']
+    url_to_project_names = data_set_project_helper
     CSV.generate(options) do |csv|
-      csv << column_names
+      csv << combined
       all.each do |data_set|
-        csv << data_set.attributes.values
+        data_set_values = data_set.attributes.values_at(*data_set_columns)
+        csv << (data_set_values + [url_to_project_names[data_set.url]])
       end
     end
   end
