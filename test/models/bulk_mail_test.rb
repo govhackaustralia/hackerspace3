b6135a271c086ea6020091151de17dac07b92344
Luke Cassar <lukecassar7@gmail.com>
2018-11-16 16:48:02 +1000

    Add BulkMail model tests.

--- test/models/bulk_mail_test.rb
+++ test/models/bulk_mail_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class BulkMailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @bulk_mail = BulkMail.first
+    @user = User.first
+    @region = Region.first
+    @team_order = TeamOrder.first
+    @user_order = UserOrder.first
+    @team_correspondence = Correspondence.first
+    @user_correspondence = Correspondence.second
+  end
+
+  test 'bulkmail associations' do
+    assert @bulk_mail.user == @user
+    assert @bulk_mail.mailable == @region
+    assert @bulk_mail.team_orders.include? @team_order
+    assert @bulk_mail.user_orders.include? @user_order
+    assert @bulk_mail.team_correspondences.include? @team_correspondence
+    assert @bulk_mail.user_correspondences.include? @user_correspondence
+  end
 end
