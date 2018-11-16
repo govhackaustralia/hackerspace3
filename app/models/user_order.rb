6ef28ebb71d50b9fa7c6efe42f76e1387be7ae72
Luke Cassar <lukecassar7@gmail.com>
2018-11-16 16:00:11 +1000

    Remove returns from case block.

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -5,13 +5,13 @@ class UserOrder < ApplicationRecord
   def registrations(event)
     case request_type
     when INVITED
-      return event.registrations.where(status: INVITED)
+      event.registrations.where(status: INVITED)
     when ATTENDING
-      return event.registrations.where(status: ATTENDING)
+      event.registrations.where(status: ATTENDING)
     when INVITED_AND_ATTENDING
-      return event.registrations.where(status: [INVITED, ATTENDING])
+      event.registrations.where(status: [INVITED, ATTENDING])
     when nil
-      return []
+      []
     end
   end
 end
