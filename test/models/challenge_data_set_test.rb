241006179982cba97e0ac685238f5de29def8541
Luke Cassar <lukecassar7@gmail.com>
2018-11-22 11:31:11 +1000

    Create new model tests for Challenge Data Set.

--- test/models/challenge_data_set_test.rb
+++ test/models/challenge_data_set_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ChallengeDataSetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @challenge_data_set = ChallengeDataSet.first
+    @challenge = Challenge.first
+    @data_set = DataSet.first
+  end
+
+  test 'challenge data set associations' do
+    assert @challenge_data_set.challenge == @challenge
+    assert @challenge_data_set.data_set == @data_set
+  end
+
+  test 'challenge data set validations' do
+    challenge_data_set = ChallengeDataSet.create(challenge: @challenge, data_set: @data_set)
+    assert_not challenge_data_set.persisted?
+  end
 end
