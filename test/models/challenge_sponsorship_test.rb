2d488faa8ef8eb478a88d0e6db70c9b1c5a10698
Luke Cassar <lukecassar7@gmail.com>
2018-11-23 16:13:18 +1000

    Add Models tests for ChallengeSponsorship

--- test/models/challenge_sponsorship_test.rb
+++ test/models/challenge_sponsorship_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ChallengeSponsorshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @challenge_sponsorship = ChallengeSponsorship.first
+    @challenge = Challenge.first
+    @sponsor = Sponsor.first
+  end
+
+  test 'challenge sponsorship associations' do
+    assert @challenge_sponsorship.challenge == @challenge
+    assert @challenge_sponsorship.sponsor == @sponsor
+  end
+
+  test 'challenge sponsorship validations' do
+    challenge_sponsorship = ChallengeSponsorship.create(sponsor: @sponsor, challenge: @challenge)
+    assert_not challenge_sponsorship.persisted?
+  end
 end
