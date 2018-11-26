c4960bef87bb5fbbdc60291b4eceabec5eda30bf
Luke Cassar <lukecassar7@gmail.com>
2018-11-26 17:33:31 +1000

    Add tests for static pages controller.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'term and conditions' do
+    get terms_and_conditions_url
+    assert_response :success
+  end
+
+  test 'code of conduct' do
+    get code_of_conduct_url
+    assert_response :success
+  end
 end
