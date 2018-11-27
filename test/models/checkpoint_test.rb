0d8b448b1bfb8bfc64b39bcebfd119c40ef1e0fb
Luke Cassar <lukecassar7@gmail.com>
2018-11-27 10:03:44 +1000

    Add tests for Checkpoint model.

--- test/models/checkpoint_test.rb
+++ test/models/checkpoint_test.rb
@@ -8,7 +8,16 @@ class CheckpointTest < ActiveSupport::TestCase
   end
 
   test 'checkpoint associations' do
-    assert(@checkpoint.competition == @competition)
-    assert(@checkpoint.entries.include?(@entry))
+    assert @checkpoint.competition == @competition
+    assert @checkpoint.entries.include? @entry
+    @checkpoint.destroy
+    assert_raises(ActiveRecord::RecordNotFound) { @entry.reload }
+  end
+
+  test 'checkpoint validations' do
+    assert_not @checkpoint.update(name: nil)
+    assert_not @checkpoint.update(end_time: nil)
+    assert_not @checkpoint.update(max_national_challenges: nil)
+    assert_not @checkpoint.update(max_regional_challenges: nil)
   end
 end
