b6138646363151775ddc29544f40fcadff8aaadb
Luke Cassar <lukecassar7@gmail.com>
2018-11-29 21:47:36 +1000

    Fix style cops in entries and users controllers

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,12 @@
 class EntriesController < ApplicationController
   def index
     @competition = Competition.current
+    retrieve_regions
+  end
+
+  private
+
+  def retrieve_regions
     passed_checkpoint_ids = @competition.passed_checkpoint_ids(LAST_TIME_ZONE)
     @region_counts = Region.national_challenges_region_counts(passed_checkpoint_ids)
     @region_names = Region.where.not(parent_id: nil).order(:name).pluck(:name)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,12 +45,7 @@ class UsersController < ApplicationController
     if params[:user].nil?
       redirect_to root_path
     else
-      @user.update(user_params) unless params.nil?
-      if @user.save
-        account_update_successfully
-      else
-        render 'edit'
-      end
+      handle_update_user
     end
   end
 
@@ -58,40 +53,60 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:full_name, :preferred_name, :preferred_img,
-                                 :tshirt_size, :twitter, :phone_number,
-                                 :mailing_list,
-                                 :challenge_sponsor_contact_place,
-                                 :challenge_sponsor_contact_enter,
-                                 :my_project_sponsor_contact,
-                                 :me_govhack_contact, :dietary_requirements,
-                                 :organisation_name,
-                                 :how_did_you_hear, :govhack_img,
-                                 :accepted_terms_and_conditions, :bsb,
-                                 :acc_number, :acc_name, :branch_name)
+                                 :tshirt_size, :twitter, :phone_number, :mailing_list,
+                                 :challenge_sponsor_contact_place, :challenge_sponsor_contact_enter,
+                                 :my_project_sponsor_contact, :me_govhack_contact, :dietary_requirements,
+                                 :organisation_name, :how_did_you_hear, :govhack_img,
+                                 :accepted_terms_and_conditions, :bsb, :acc_number, :acc_name, :branch_name)
+  end
+
+  def handle_update_user
+    @user.update(user_params) unless params.nil?
+    if @user.save
+      account_update_successfully
+    else
+      render 'edit'
+    end
   end
 
   def account_update_successfully
     if params[:user][:accepted_terms_and_conditions]
-      flash[:notice] = 'Welcome! Please complete your registration.'
-      redirect_to complete_registration_path
+      handle_complete_registration
     elsif @account_registration
       handle_end_of_registration
     elsif params[:user][:govhack_img].present?
-      flash[:notice] = 'GovHack Profile Uploaded'
-      render :edit, profile_pic: true
+      handle_profile_img
     elsif params[:banking].present?
-      BankMailer.notify_finance(@user).deliver_later
-      flash[:notice] = 'Your banking details have been updated.'
-      redirect_to manage_account_path
+      handle_bank_update
     else
-      flash[:notice] = 'Your personal details have been updated.'
-      redirect_to manage_account_path
+      handle_personal_details
     end
+  end
+
+  def handle_complete_registration
+    flash[:notice] = 'Welcome! Please complete your registration.'
+    redirect_to complete_registration_path
   end
 
   def handle_end_of_registration
     current_user.update(how_did_you_hear: NO_RESPONSE) if @user.how_did_you_hear.empty?
     flash[:notice] = 'Your account has been created.'
+    redirect_to manage_account_path
+  end
+
+  def handle_profile_img
+    flash[:notice] = 'GovHack Profile Uploaded'
+    render :edit, profile_pic: true
+  end
+
+  def handle_bank_update
+    BankMailer.notify_finance(@user).deliver_later
+    flash[:notice] = 'Your banking details have been updated.'
+    redirect_to manage_account_path
+  end
+
+  def handle_personal_details
+    flash[:notice] = 'Your personal details have been updated.'
     redirect_to manage_account_path
   end
 end
