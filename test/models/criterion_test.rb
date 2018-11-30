44f06653f56650e50d6acd4273bf6ed36c9b1c29
Luke Cassar <lukecassar7@gmail.com>
2018-11-30 11:54:03 +1000

    Update models tests on Criterion model.

--- test/models/criterion_test.rb
+++ test/models/criterion_test.rb
@@ -4,10 +4,20 @@ class CriterionTest < ActiveSupport::TestCase
   setup do
     @criterion = Criterion.first
     @competition = Competition.first
-    @entry = Entry.first
+    @judgment = Judgment.first
   end
 
   test 'criterion associations' do
-    assert(@criterion.competition == @competition)
+    assert @criterion.competition == @competition
+    assert @criterion.judgments.include? @judgment
+  end
+
+  test 'criterion validations' do
+    assert_not @criterion.update(description: nil)
+    assert @criterion.update(description: 'Test')
+    assert_not @criterion.update(name: nil)
+    assert @criterion.update(name: 'Test')
+    assert_not @criterion.update(category: 'Test')
+    assert @criterion.update!(category: JUDGEMENT_CATEGORIES.sample)
   end
 end
