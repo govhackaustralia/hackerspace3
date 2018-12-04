8c502c285b406b92732cba6449e411858a59a478
Luke Cassar <lukecassar7@gmail.com>
2018-12-04 17:50:04 +1000

    Add comments to checkpoint model.

--- app/models/checkpoint.rb
+++ app/models/checkpoint.rb
@@ -5,6 +5,8 @@ class Checkpoint < ApplicationRecord
   validates :name, :end_time, :max_national_challenges,
             :max_regional_challenges, presence: true
 
+  # Returns a boolean based on whether a team has reached the maximum
+  # number of challenges for a particular region.
   def limit_reached?(team, region)
     if region.national?
       max = max_national_challenges
@@ -13,11 +15,11 @@ class Checkpoint < ApplicationRecord
       max = max_regional_challenges
       entry_count = team.regional_challenges(self).count
     end
-    return false if entry_count < max
-
-    true
+    entry_count >= max
   end
 
+  # Returns a boolean based on whether a checkpoint has passed in a given
+  # time zone.
   def passed?(time_zone)
     end_time.to_formatted_s(:number) < Time.now.in_time_zone(time_zone).to_formatted_s(:number)
   end
