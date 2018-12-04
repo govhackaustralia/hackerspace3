fee136c5c31b47651e4f92c3f5fcc6729327381e
Luke Cassar <lukecassar7@gmail.com>
2018-12-04 11:32:35 +1000

    Add validation and more association tests to Entry model.

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -6,11 +6,24 @@ class EntryTest < ActiveSupport::TestCase
     @checkpoint = Checkpoint.first
     @challenge = Challenge.first
     @team = Team.first
+    @competition = Competition.first
+    @scorecard = Scorecard.first
   end
 
   test 'entry associations' do
-    assert(@entry.checkpoint == @checkpoint)
-    assert(@entry.challenge == @challenge)
-    assert(@entry.team == @team)
+    assert @entry.checkpoint == @checkpoint
+    assert @entry.challenge == @challenge
+    assert @entry.team == @team
+    assert @entry.competition == @competition
+    assert @entry.scorecards.include? @scorecard
+    @entry.destroy
+    assert_raises(ActiveRecord::RecordNotFound) { @scorecard.reload }
+  end
+
+  test 'entry validations' do
+    # Justification cannot be nil
+    assert_not @entry.update(justification: nil)
+    # No duplicate entries by Team into Challenge.
+    assert_not Entry.second.update(challenge: @challenge)
   end
 end
