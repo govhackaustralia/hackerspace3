8a777d7a1c003c719cd36706f637f83ebfff51d3
Luke Cassar <lukecassar7@gmail.com>
2018-12-10 09:37:20 +1000

    Add tests to flight model.

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @flight = Flight.first
+    @event = Event.first
+    @registration_flight = RegistrationFlight.first
+  end
+
+  test 'flight associations' do
+    assert @flight.event == @event
+    assert @flight.registration_flights.include? @registration_flight
+  end
+
+  test 'flight validations' do
+    assert_not @flight.update direction: 'Test'
+  end
 end
