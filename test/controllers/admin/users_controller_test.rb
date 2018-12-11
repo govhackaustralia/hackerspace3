289de24806fe17a57b2b6eb80a007af3ccaa0a60
Luke Cassar <lukecassar7@gmail.com>
2018-12-11 11:46:51 +1000

    Add controller test for user controller.

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = User.first
+    sign_in users(:one)
+  end
+
+  test 'should get show' do
+    get manage_account_url
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get update_personal_details_path
+    assert_response :success
+  end
+
+  test 'should patch update' do
+    patch user_path(@user), params: { user: { full_name: 'updated' } }
+    assert_redirected_to manage_account_url
+    @user.reload
+    assert @user.full_name == 'updated'
+  end
 end
