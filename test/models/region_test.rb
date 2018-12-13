9342200726f20aed63a123b795e9b68b239e4be9
Luke Cassar <lukecassar7@gmail.com>
2018-12-13 10:06:02 +1000

    Add more association test to region model.

--- test/models/region_test.rb
+++ test/models/region_test.rb
@@ -4,37 +4,48 @@ class RegionTest < ActiveSupport::TestCase
   setup do
     @parent_region = Region.first
     @child_region = Region.second
-    @event = Event.first
     @assignment = Assignment.second
-    @user = User.first
+    @event = Event.first
+    @team = Team.first
+    @published_project = Project.first
+    @entry = Entry.first
     @sponsorship = Sponsorship.second
+    @sponsorship_type = SponsorshipType.first
     @challenge = Challenge.first
+    @data_set = DataSet.first
+    @bulk_mail = BulkMail.first
   end
 
   test 'region associations' do
-    assert(@parent_region.sub_regions.include?(@child_region))
-    assert(@child_region.parent == @parent_region)
-    assert(@parent_region.assignments.include?(@assignment))
-    assert(@child_region.events.include?(@event))
-    assert(@parent_region.sponsorships.include?(@sponsorship))
-    assert(@parent_region.challenges.include?(@challenge))
+    assert @child_region.parent == @parent_region
+    assert @parent_region.sub_regions.include? @child_region
+    assert @parent_region.assignments.include? @assignment
+    assert @child_region.events.include? @event
+    assert @child_region.teams.include? @team
+    assert @child_region.published_projects_by_name.include? @published_project
+    assert @child_region.entries.include? @entry
+    assert @parent_region.sponsorships.include? @sponsorship
+    assert @parent_region.sponsorship_types.include? @sponsorship_type
+    assert @parent_region.challenges.include? @challenge
+    assert @parent_region.data_sets.include? @data_set
+    assert @parent_region.bulk_mails.include? @bulk_mail
   end
 
   test 'region validations' do
     # No name given
-    assert_not(Region.create.persisted?)
+    assert_not Region.create.persisted?
     # Duplicate name
-    assert_not(Region.create(name: @parent_region.name).persisted?)
+    assert_not Region.create(name: @parent_region.name).persisted?
     # Time zone not required
-    assert(Region.create(name: 'y').persisted?)
+    assert Region.create(name: 'y').persisted?
     # Incorrect Region
-    assert_not(Region.create(name: 'x', time_zone: 'Timbuktu').persisted?)
+    assert_not Region.create(name: 'x', time_zone: 'Timbuktu').persisted?
     # Correct Region
-    assert(Region.create(name: 'x', time_zone: 'Brisbane').persisted?)
+    assert Region.create(name: 'x', time_zone: 'Brisbane').persisted?
   end
 
   test 'region director' do
     # Retrievs Director
-    assert(@assignment.user == @parent_region.director)
+    assert @assignment.user == @parent_region.director
   end
 end
