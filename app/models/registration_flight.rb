8a8fe8b982926af91d0dc53b56daf7011de295e9
Luke Cassar <lukecassar7@gmail.com>
2018-12-14 11:54:44 +1000

    Add model tests and extra validation to registration flight model.

--- app/models/registration_flight.rb
+++ app/models/registration_flight.rb
@@ -1,4 +1,6 @@
 class RegistrationFlight < ApplicationRecord
   belongs_to :flight
   belongs_to :registration
+
+  validates :flight, uniqueness: { scope: :registration, message: 'Flight Reservation already exists.' }
 end
--- test/models/registration_flight_test.rb
+++ test/models/registration_flight_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RegistrationFlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @registration_flight = RegistrationFlight.first
+    @flight = Flight.first
+    @registration = Registration.first
+  end
+
+  test 'registration flight associations' do
+    assert @registration_flight.flight == @flight
+    assert @registration_flight.registration == @registration
+  end
+
+  test 'registration flight validations' do
+    assert_not @registration_flight.update flight_id: 2
+  end
 end
