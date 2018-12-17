0f254dd028b2f596d5cdfc67bbd1b3ca0959a6a3
Luke Cassar <lukecassar7@gmail.com>
2018-12-17 11:17:29 +1000

    Add controller test to Teams Controller.

--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class TeamsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:one)
+    Competition.current.update(start_time: Time.now.yesterday, end_time: Time.now.tomorrow)
+  end
+
+  test 'should get new' do
+    get new_team_url
+    assert_response :success
+  end
+
+  test 'should post create' do
+    assert_difference('Team.count') do
+      post teams_url params: { team: { event_id: 2, youth_team: false } }
+    end
+    assert_redirected_to team_management_team_path(Team.last)
+  end
 end
