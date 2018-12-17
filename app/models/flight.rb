51f4525cdd2f516bc29a0468d0a22fa863689e14
Luke Cassar <lukecassar7@gmail.com>
2018-12-17 10:56:26 +1000

    Add scopes to flight classes and test.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :event
   has_many :registration_flights, dependent: :destroy
+
+  scope :inbound, -> { find_by(direction: INBOUND) }
+  scope :outbound, -> { find_by(direction: OUTBOUND) }
+
   validates :direction, inclusion: { in: FLIGHT_DIRECTIONS }
 end
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -5,11 +5,18 @@ class FlightTest < ActiveSupport::TestCase
     @flight = Flight.first
     @event = Event.first
     @registration_flight = RegistrationFlight.first
+    @inbound = Flight.first
+    @outbound = Flight.second
   end
 
   test 'flight associations' do
     assert @flight.event == @event
     assert @flight.registration_flights.include? @registration_flight
+  end
+
+  test 'flight scopes' do
+    assert Flight.inbound == @inbound
+    assert Flight.outbound == @outbound
   end
 
   test 'flight validations' do
