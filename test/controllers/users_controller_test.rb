426cd6b266c424c10e6968fdf3364c4bc79c14ff
Luke Cassar <lukecassar7@gmail.com>
2018-12-18 12:33:14 +1000

    Add controller test to user controller.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:one)
+  end
+
+  test 'should get show' do
+    get manage_account_url
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get update_personal_details_url
+    assert_response :success
+  end
+
+  # FIX: Should be testing update, but want it broken up into smaller
+  # controllers first
 end
