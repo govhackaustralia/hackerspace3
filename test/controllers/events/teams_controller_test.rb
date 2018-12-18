8036316382ff722fe7ddd54262ebaa3580f01033
Luke Cassar <lukecassar7@gmail.com>
2018-12-19 09:46:01 +1000

    Add controller test to public event/teams.

--- test/controllers/events/teams_controller_test.rb
+++ test/controllers/events/teams_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class Events::TeamsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @event = Event.first
+  end
+
+  test 'should get index public user' do
+    get event_teams_url(@event.identifier)
+    assert_response :success
+  end
+
+  test 'should get index authenticated user' do
+    sign_in users(:one)
+    get event_teams_url(@event.identifier)
+    assert_response :success
+  end
 end
