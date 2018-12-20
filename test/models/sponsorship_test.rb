1f85f043ffda0d0440c7bd5c870f6e8a7a01cb19
Luke Cassar <lukecassar7@gmail.com>
2018-12-20 10:37:11 +1000

    Add tests to sponsorship model.

--- test/models/sponsorship_test.rb
+++ test/models/sponsorship_test.rb
@@ -7,11 +7,13 @@ class SponsorshipTest < ActiveSupport::TestCase
     @sponsor = Sponsor.first
     @event = Event.first
     @region = Region.first
+    @sponsorship_type = SponsorshipType.first
   end
 
   test 'sponsorship associations' do
-    assert(@event_sponsorship.sponsor == @sponsor)
-    assert(@region_sponsorship.sponsorable == @region)
-    assert(@event_sponsorship.sponsorable == @event)
+    assert @event_sponsorship.sponsor == @sponsor
+    assert @region_sponsorship.sponsorable == @region
+    assert @event_sponsorship.sponsorable == @event
+    assert @region_sponsorship.sponsorship_type == @sponsorship_type
   end
 end
