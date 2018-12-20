bd77f7134592b41383d6d7c6463b5b1ab36778a1
Luke Cassar <lukecassar7@gmail.com>
2018-12-20 16:13:26 +1000

    Add comments to project model.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :team
-  has_one :event, through: :team
   belongs_to :user
+
+  has_one :event, through: :team
 
   validates :team_name, :project_name, presence: true
 
@@ -9,16 +10,14 @@ class Project < ApplicationRecord
 
   after_create :update_team_current_project
 
+  # Updates each of the entries that a team has entered and determines if the
+  # team is eligible or ineligible.
   def update_entries_eligible
     team.entries.each { |entry| entry.update_eligible(self) }
   end
 
-  def self.id_projects(projects)
-    id_projects = {}
-    projects.each { |project| id_projects[project.id] = project }
-    id_projects
-  end
-
+  # Updates the Project Identifier and checks for uniqueness.
+  # ENHANCEMENT: Should be moved to Team object.
   def update_identifier
     new_identifier = uri_pritty(project_name)
     new_identifier = uri_pritty("#{project_name}-#{team.id}") if already_there?(new_identifier)
@@ -27,10 +26,13 @@ class Project < ApplicationRecord
 
   private
 
+  # Make the latest proect created the current project.
   def update_team_current_project
     team.update(current_project: self)
   end
 
+  # Checks to see if an identifier has been taken.
+  # ENHANCEMENT: Move to active record validations.
   def already_there?(new_identifier)
     project = Project.find_by(identifier: new_identifier)
     return false if project.nil?
@@ -39,6 +41,8 @@ class Project < ApplicationRecord
     true
   end
 
+  # Remove non URI complient strings.
+  # ENHANCEMENT: Search for standard library.
   def uri_pritty(string)
     array = string.split(/\W/)
     words = array - ['']
