3bd67a6d69d6c8ac10f258bf92bf8d09192f7acb
Luke Cassar <lukecassar7@gmail.com>
2018-12-22 12:49:41 +1000

    Add model test to Team Data Set model.

--- test/models/team_data_set_test.rb
+++ test/models/team_data_set_test.rb
@@ -4,9 +4,15 @@ class TeamDataSetTest < ActiveSupport::TestCase
   setup do
     @team = Team.first
     @team_data_set = TeamDataSet.first
+    @current_project = Project.first
   end
 
   test 'TeamDataSet Associations' do
-    assert(@team_data_set.team == @team)
+    assert @team_data_set.team == @team
+    assert @team_data_set.current_project == @current_project
+  end
+
+  test 'TeamDataSet Validations' do
+    assert_not @team_data_set.update name: nil
   end
 end
