8f0f647a7b84bfea8035170a31eb3e62ff160eae
Luke Cassar <lukecassar7@gmail.com>
2018-12-23 10:03:00 +1000

    Add model tests to TeamOrder model.

--- test/models/team_order_test.rb
+++ test/models/team_order_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TeamOrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @team_order = TeamOrder.first
+    @bulk_mail = BulkMail.first
+    @team = Team.first
+    @correspondence = Correspondence.first
+  end
+
+  test 'team order associations' do
+    assert @team_order.bulk_mail == @bulk_mail
+    assert @team_order.team == @team
+    assert @team_order.correspondences.include? @correspondence
+  end
+
+  test 'team validations' do
+    assert @team_order.update request_type: TEAM_ORDER_REQUEST_TYPES.sample
+    assert_not @team_order.update request_type: 'TEST'
+    assert_not @team_order.update team_id: 2
+  end
 end
