fd5d097000f7f5c9d29dae1b1b047724311c8819
Luke Cassar <lukecassar7@gmail.com>
2019-01-08 12:13:37 +1000

    Add tests to UserOrder model.

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,5 +1,6 @@
 class UserOrder < ApplicationRecord
   belongs_to :bulk_mail
+
   has_many :correspondences, as: :orderable, dependent: :destroy
 
   def registrations(event)
--- test/models/user_order_test.rb
+++ test/models/user_order_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserOrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_order = UserOrder.first
+    @bulk_mail = BulkMail.second
+    @correspondence = Correspondence.second
+  end
+
+  test 'user associations' do
+    assert @user_order.bulk_mail == @bulk_mail
+    assert @user_order.correspondences.include? @correspondence
+  end
 end
