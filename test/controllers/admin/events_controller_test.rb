133673c0d253a9cf1bc1c502ba2bfb25937f93c2
Luke Cassar <lukecassar7@gmail.com>
2019-01-18 11:13:55 +1000

    Add test for admin events controller.

--- test/controllers/admin/events_controller_test.rb
+++ test/controllers/admin/events_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class Admin::EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:one)
+  end
+
+  test 'should get index' do
+    get admin_events_url
+    assert_response :success
+  end
 end
