066750c4901413896d3663b81cd4746930da6ec5
Luke Cassar <lukecassar7@gmail.com>
2019-01-20 16:24:03 +1000

    Add admin projects controller test.

--- test/controllers/admin/projects_controller_test.rb
+++ test/controllers/admin/projects_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class Admin::ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users(:one)
+    @team = Team.first
+    @project = Project.first
+  end
+
+  test 'should get index' do
+    get admin_team_projects_url @team
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get admin_team_project_url @team, @project
+    assert_response :success
+  end
 end
