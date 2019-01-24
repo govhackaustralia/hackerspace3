2a9eb599246e3c41a61e09856a3f54f0fc680faa
Luke Cassar <lukecassar7@gmail.com>
2019-01-24 12:02:14 +1000

    Remove redundant id_user_correspondences method in Correspondence model.

--- app/models/correspondence.rb
+++ app/models/correspondence.rb
@@ -3,13 +3,4 @@ class Correspondence < ApplicationRecord
   belongs_to :user
 
   validates :status, inclusion: { in: CORRESPONDENCE_STATUS_TYPES }
-
-  # ENHANCEMENT: Add to Helper.
-  def self.id_user_correspondences(bulk_mail)
-    id_user_correspondences = {}
-    bulk_mail.correspondences.each do |correspondence|
-      id_user_correspondences[correspondence.user_id] = correspondence
-    end
-    id_user_correspondences
-  end
 end
