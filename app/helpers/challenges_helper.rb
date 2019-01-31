3c76ff16256cc88005b9108c3df76496c4bf83f3
Luke Cassar <lukecassar7@gmail.com>
2019-01-31 11:55:44 +1000

    Add comments to challenges and competitions helpers.

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,4 +1,6 @@
 module ChallengesHelper
+  # Returns all published teams entered into a a challenge at a particular
+  # checkpoint.
   # ENHANCEMENT: DB queries made, this should be in the controller.
   def challenge_teams(checkpoint)
     team_ids = @challenge.entries_at(checkpoint).pluck(:team_id)
--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,8 +1,12 @@
 module CompetitionsHelper
+  # Returns true if the competition has started or if a user has region
+  # privileges
   def competition_started_or_region_privileges?(time_zone = nil)
     @competition.started?(time_zone) || @region_privileges
   end
 
+  # Returns true if a competition window is open or a user has region
+  # privileges
   def in_competition_window_or_region_privileges?(time_zone = nil)
     @competition.in_window?(time_zone) || @region_privileges
   end
