104c7fddd13624e8c8759dd9ecc1e53a238c17f6
Luke Cassar <lukecassar7@gmail.com>
2019-01-31 10:08:38 +1000

    Add test to admin bulkmail correspondence controller.

--- test/controllers/admin/bulk_mails/correspondences_controller_test.rb
+++ test/controllers/admin/bulk_mails/correspondences_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class Admin::BulkMails::CorrespondencesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :one
+    @correspondence = Correspondence.first
+    @bulk_mail = BulkMail.first
+  end
+
+  test 'should get show' do
+    get admin_bulk_mail_correspondence_url @bulk_mail, @correspondence
+    assert_response :success
+  end
 end
