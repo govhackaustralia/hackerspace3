6c2441d3b4fefb5b628d4950429f663ff9d3e514
Luke Cassar <lukecassar7@gmail.com>
2019-02-02 12:08:30 +1000

    Add admin challenge data set create fail controller test.

--- test/controllers/admin/challenges/challenge_data_sets_controller_test.rb
+++ test/controllers/admin/challenges/challenge_data_sets_controller_test.rb
@@ -2,29 +2,37 @@ require 'test_helper'
 
 class Admin::Challenges::ChallengeDataSetsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    sign_in users(:one)
+    sign_in users :one
     @challenge = Challenge.first
     @data_set = DataSet.first
     @challenge_data_set = ChallengeDataSet.first
   end
 
   test 'should get new' do
-    get new_admin_challenge_challenge_data_set_url(@challenge)
+    get new_admin_challenge_challenge_data_set_url @challenge
     assert_response :success
   end
 
-  test 'should post create' do
+  test 'should post create success' do
     ChallengeDataSet.destroy_all
-    assert_difference('ChallengeDataSet.count') do
-      post admin_challenge_challenge_data_sets_url(@challenge), params: { challenge_data_set: { data_set_id: @data_set.id } }
+    assert_difference 'ChallengeDataSet.count' do
+      post admin_challenge_challenge_data_sets_url @challenge, params: { challenge_data_set: { data_set_id: @data_set.id } }
     end
-    assert_redirected_to admin_region_challenge_url(@challenge.region_id, @challenge)
+    assert_redirected_to admin_region_challenge_url @challenge.region_id, @challenge
+  end
+
+  test 'should post create fail' do
+    ChallengeDataSet.destroy_all
+    assert_no_difference 'ChallengeDataSet.count' do
+      post admin_challenge_challenge_data_sets_url @challenge, params: { challenge_data_set: { data_set_id: nil } }
+    end
+    assert_response :success
   end
 
   test 'should delete destroy' do
-    assert_difference('ChallengeDataSet.count', -1) do
-      delete admin_challenge_challenge_data_set_url(@challenge, @challenge_data_set)
+    assert_difference 'ChallengeDataSet.count', -1 do
+      delete admin_challenge_challenge_data_set_url @challenge, @challenge_data_set
     end
-    assert_redirected_to admin_region_challenge_url(@challenge.region_id, @challenge)
+    assert_redirected_to admin_region_challenge_url @challenge.region_id, @challenge
   end
 end
