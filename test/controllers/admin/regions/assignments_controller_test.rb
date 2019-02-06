0d7660e3db904b34b377d24973160d4097576b89
Luke Cassar <lukecassar7@gmail.com>
2019-02-06 10:57:01 +1000

    Add controller test to admin region assignments.

--- test/controllers/admin/regions/assignments_controller_test.rb
+++ test/controllers/admin/regions/assignments_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class Admin::Regions::AssignmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :one
+    @region = Event.first
+    @user = User.first
+  end
+
+  test 'should get index' do
+    get admin_region_assignments_url @region
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_admin_region_assignment_url @region, title: REGION_DIRECTOR
+    assert_response :success
+    get new_admin_region_assignment_url @region, title: REGION_DIRECTOR, term: 'x'
+    assert_response :success
+    get new_admin_region_assignment_url @region, title: REGION_DIRECTOR, term: 'a'
+    assert_response :success
+  end
+
+  test 'should post create success' do
+    assert_difference 'Assignment.count' do
+      post admin_region_assignments_url @region, params: { title: REGION_DIRECTOR, user_id: @user }
+    end
+    assert_redirected_to admin_region_assignments_url @region
+  end
+
+  test 'should post create fail' do
+    assert_no_difference 'Assignment.count' do
+      post admin_region_assignments_url @region, params: { title: REGION_DIRECTOR, user_id: nil }
+    end
+    assert_response :success
+  end
 end
