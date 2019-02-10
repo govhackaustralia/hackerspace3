612fceea07692b58f6165706909ac6eefa208d35
Luke Cassar <lukecassar7@gmail.com>
2019-02-11 09:39:12 +1000

    Add controller test for admin sponsor assignments.

--- app/controllers/admin/sponsors/assignments_controller.rb
+++ app/controllers/admin/sponsors/assignments_controller.rb
@@ -2,18 +2,18 @@ class Admin::Sponsors::AssignmentsController < Admin::AssignmentsController
   before_action :authenticate_user!
 
   def new
-    @assignable = Sponsor.find(params[:sponsor_id])
+    @assignable = Sponsor.find params[:sponsor_id]
     handle_new
   end
 
   def create
-    @assignable = Sponsor.find(params[:sponsor_id])
+    @assignable = Sponsor.find params[:sponsor_id]
     handle_create
   end
 
   private
 
   def redirect_to_index
-    redirect_to admin_sponsor_path(@assignable)
+    redirect_to admin_sponsor_path @assignable
   end
 end
--- test/controllers/admin/sponsors/assignments_controller_test.rb
+++ test/controllers/admin/sponsors/assignments_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class Admin::Sponsors::AssignmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :one
+    @sponsor = Sponsor.first
+    @user = User.first
+  end
+
+  test 'should get new' do
+    get new_admin_sponsor_assignment_url @sponsor, title: SPONSOR_CONTACT
+    assert_response :success
+    get new_admin_sponsor_assignment_url @sponsor, title: SPONSOR_CONTACT, term: 'x'
+    assert_response :success
+    get new_admin_sponsor_assignment_url @sponsor, title: SPONSOR_CONTACT, term: 'a'
+    assert_response :success
+  end
+
+  test 'should post create success' do
+    assert_difference 'Assignment.count' do
+      post admin_sponsor_assignments_url @sponsor, params: { title: SPONSOR_CONTACT, user_id: @user }
+    end
+    assert_redirected_to admin_sponsor_url @sponsor
+  end
+
+  test 'should post create fail' do
+    assert_no_difference 'Assignment.count' do
+      post admin_sponsor_assignments_url @sponsor, params: { title: SPONSOR_CONTACT, user_id: nil }
+    end
+    assert_response :success
+  end
 end
