3edcaaa72ff9be6737c39519f0ba6e8d247b0e0d
Luke Cassar <lukecassar7@gmail.com>
2019-02-11 10:29:10 +1000

    Add controller test for admin team entries.

--- app/controllers/admin/teams/entries_controller.rb
+++ app/controllers/admin/teams/entries_controller.rb
@@ -3,45 +3,49 @@ class Admin::Teams::EntriesController < ApplicationController
   before_action :check_for_privileges
 
   def new
-    @team = Team.find(params[:team_id])
+    @team = Team.find params[:team_id]
     @entry = @team.entries.new
-    @challenges = @team.available_challenges(params[:challenge_type])
+    @challenges = @team.available_challenges params[:challenge_type]
+  end
+
+  def create
+    @team = Team.find params[:team_id]
+    @entry = @team.entries.new entry_params
+    if @entry.save
+      flash[:notice] = 'New Challenge Entry Created'
+      redirect_to admin_team_path @team
+    else
+      handle_create_fail
+    end
   end
 
   def edit
-    @entry = Entry.find(params[:id])
+    @entry = Entry.find params[:id]
     @team = @entry.team
     @challenge = @entry.challenge
     @checkpoints = (@team.admin_available_checkpoints(@challenge) << @entry.checkpoint).uniq
   end
 
   def update
-    @entry = Entry.find(params[:id])
+    @entry = Entry.find params[:id]
     @challenge = @entry.challenge
     @team = @entry.team
     @checkpoints = (@team.admin_available_checkpoints(@challenge) << @entry.checkpoint).uniq
     handle_update
   end
 
-  def create
-    @team = Team.find(params[:team_id])
-    @entry = @team.entries.new(entry_params)
-    @entry.checkpoint_id = params[:checkpoint_id]
-    handle_create
-  end
-
   def destroy
-    @team = Team.find(params[:team_id])
-    @entry = Entry.find(params[:id])
+    @team = Team.find params[:team_id]
+    @entry = Entry.find params[:id]
     @entry.destroy
     flash[:notice] = 'Challenge Entry Removed'
-    redirect_to admin_team_path(@team)
+    redirect_to admin_team_path @team
   end
 
   private
 
   def entry_params
-    params.require(:entry).permit(:challenge_id, :justification, :checkpoint_id)
+    params.require(:entry).permit :challenge_id, :justification, :checkpoint_id
   end
 
   def check_for_privileges
@@ -51,25 +55,16 @@ class Admin::Teams::EntriesController < ApplicationController
     redirect_to root_path
   end
 
-  def handle_create
-    if @entry.save
-      flash[:notice] = 'New Challenge Entry Created'
-      redirect_to admin_team_path(@team)
-    else
-      handle_create_fail
-    end
-  end
-
   def handle_create_fail
-    @challenges = @team.available_challenges(params[:challenge_type])
+    @challenges = @team.available_challenges params[:challenge_type]
     flash[:alert] = @entry.errors.full_messages.to_sentence
     render :new, challenge_type: params[:challenge_type], checkpoint_id: params[:checkpoint_id]
   end
 
   def handle_update
-    if @entry.update(entry_params)
+    if @entry.update entry_params
       flash[:notice] = 'Entry Updated Successfully'
-      redirect_to admin_team_path(@team)
+      redirect_to admin_team_path @team
     else
       flash[:alert] = @entry.errors.full_messages.to_sentence
       render :edit
--- test/controllers/admin/teams/entries_controller_test.rb
+++ test/controllers/admin/teams/entries_controller_test.rb
@@ -1,7 +1,69 @@
 require 'test_helper'
 
 class Admin::Teams::EntriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :one
+    @team = Team.first
+    @entry = Entry.first
+    @challenge = Challenge.first
+    @checkpoint = Checkpoint.first
+  end
+
+  test 'should get new' do
+    get new_admin_team_entry_url @team
+    assert_response :success
+  end
+
+  test 'should post create success' do
+    Entry.destroy_all
+    assert_difference 'Entry.count' do
+      post admin_team_entries_url @team, params: { entry: {
+        challenge_id: @challenge.id,
+        checkpoint_id: @checkpoint.id,
+        justification: 'Test'
+      } }
+    end
+    assert_redirected_to admin_team_url @team
+  end
+
+  test 'should post create fail' do
+    assert_no_difference 'Entry.count' do
+      post admin_team_entries_url @team, params: { entry: {
+        challenge_id: @challenge.id,
+        checkpoint_id: @checkpoint.id,
+        justification: 'Test'
+      } }
+    end
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get edit_admin_team_entry_url @team, @entry
+    assert_response :success
+  end
+
+  test 'should patch update success' do
+    patch admin_team_entry_url @team, @entry, params: { entry: {
+      justification: 'Updated'
+    } }
+    assert_redirected_to admin_team_url @team
+    @entry.reload
+    assert @entry.justification == 'Updated'
+  end
+
+  test 'should patch update fail' do
+    patch admin_team_entry_url @team, @entry, params: { entry: {
+      justification: nil
+    } }
+    assert_response :success
+    @entry.reload
+    assert_not @entry.justification.nil?
+  end
+
+  test 'should delete destroy' do
+    assert_difference 'Entry.count', -1 do
+      delete admin_team_entry_url @team, @entry
+    end
+    assert_redirected_to admin_team_url @team
+  end
 end
