b5395b80b02371c2af110db7f8128443ca0faf73
Luke Cassar <lukecassar7@gmail.com>
2019-02-11 10:59:39 +1000

    Add controller test to admin team scorecards.

--- app/controllers/admin/teams/scorecards_controller.rb
+++ app/controllers/admin/teams/scorecards_controller.rb
@@ -2,8 +2,10 @@ class Admin::Teams::ScorecardsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_for_privileges
 
+  require 'descriptive_statistics'
+
   def index
-    @team = Team.find(params[:team_id])
+    @team = Team.find params[:team_id]
     @project = @team.current_project
     @region = @team.region
     @competition = @team.competition
@@ -11,16 +13,16 @@ class Admin::Teams::ScorecardsController < ApplicationController
   end
 
   def update
-    @scorecard = Scorecard.find(params[:id])
-    @scorecard.update(included: !@scorecard.included)
-    redirect_to admin_team_scorecards_path(@scorecard.judgeable, popup: true, include_judges: params[:include_judges])
+    @scorecard = Scorecard.find params[:id]
+    @scorecard.update included: !@scorecard.included
+    redirect_to admin_team_scorecards_path @scorecard.judgeable, popup: true, include_judges: params[:include_judges]
   end
 
   def destroy
-    @scorecard = Scorecard.find(params[:id])
+    @scorecard = Scorecard.find params[:id]
     @scorecard.destroy
     flash[:notice] = 'Scorecard Deleted'
-    redirect_to admin_team_scorecards_path(@scorecard.judgeable, popup: true, include_judges: params[:include_judges])
+    redirect_to admin_team_scorecards_path @scorecard.judgeable, popup: true, include_judges: params[:include_judges]
   end
 
   private
@@ -40,7 +42,7 @@ class Admin::Teams::ScorecardsController < ApplicationController
   # Retrieves all a team's scorecards and removes those of the judges if
   # params require.
   def participant_scorecards
-    all_scorecards = @team.scorecards.order(:assignment_id).preload(:assignment_scorecards, :assignment_judgments, :judgments)
+    all_scorecards = @team.scorecards.order(:assignment_id).preload :assignment_scorecards, :assignment_judgments, :judgments
     return all_scorecards if params[:include_judges] == true.to_s
 
     all_scorecards - @team.judge_scorecards.where(judgeable: @team)
--- test/controllers/admin/teams/scorecards_controller_test.rb
+++ test/controllers/admin/teams/scorecards_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class Admin::Teams::ScorecardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :one
+    @team = Team.first
+    @scorecard = Scorecard.first
+  end
+
+  test 'should get index' do
+    get admin_team_scorecards_url @team
+    assert_response :success
+  end
+
+  test 'should patch update' do
+    patch admin_team_scorecard_url @team, @scorecard, popup: true, include_judges: true
+    assert_redirected_to admin_team_scorecards_url @team, popup: true, include_judges: true
+    @scorecard.reload
+    assert_not @scorecard.included
+  end
+
+  test 'should delete destroy' do
+    assert_difference 'Scorecard.count', -1 do
+      delete admin_team_scorecard_url @team, @scorecard, popup: true, include_judges: true
+    end
+    assert_redirected_to admin_team_scorecards_url @team, popup: true, include_judges: true
+  end
 end
