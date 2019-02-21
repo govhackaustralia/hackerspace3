87ced0fd489c21424022261ba24323279f98f87c
Luke Cassar <lukecassar7@gmail.com>
2019-02-21 17:26:26 +1000

    Add controller test for users memberships.

--- test/controllers/users/memberships_controller_test.rb
+++ test/controllers/users/memberships_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class Users::MembershipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :two
+    @membership = Assignment.find(9)
+  end
+
+  test 'should delete destroy' do
+    assert_difference 'Assignment.count', -1 do
+      delete users_membership_path @membership
+    end
+    assert_redirected_to manage_account_path
+  end
 end
