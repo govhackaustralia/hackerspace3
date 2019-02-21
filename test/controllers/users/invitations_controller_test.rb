931838efe566dd2d6763fe50395c2cceb2047a1a
Luke Cassar <lukecassar7@gmail.com>
2019-02-21 17:25:54 +1000

    Add controller test for users invitations.

--- test/controllers/users/invitations_controller_test.rb
+++ test/controllers/users/invitations_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class Users::InvitationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    sign_in users :three
+    @invitation = Assignment.find(12)
+  end
+
+  test 'should patch update' do
+    patch users_invitation_path @invitation
+    assert_redirected_to team_management_team_url @invitation.assignable
+    @invitation.reload
+    assert @invitation.title == TEAM_MEMBER
+  end
+
+  test 'should delete destroy' do
+    assert_difference 'Assignment.count', -1 do
+      delete users_invitation_path @invitation
+    end
+    assert_redirected_to manage_account_path
+  end
 end
