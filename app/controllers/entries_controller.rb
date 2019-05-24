bee3dfada72de9785ca06febd590afbc5d85d579
Luke Cassar <luke.cassar@envisionapp.com>
2019-05-24 20:47:45 +1000

    Refactor entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,10 +7,10 @@ class EntriesController < ApplicationController
   private
 
   def retrieve_regions
-    passed_checkpoint_ids = @competition.passed_checkpoint_ids(LAST_TIME_ZONE)
-    @region_counts = helpers.challenges_region_counts(passed_checkpoint_ids)
-    @region_names = Region.where.not(parent_id: nil).order(:name).pluck(:name)
-    @challenge_names = Region.root(@competition).challenges.order(:name).pluck(:name)
-    @sub_regions = Region.where.not(parent_id: nil).order(:name)
+    passed_checkpoint_ids = @competition.passed_checkpoint_ids LAST_TIME_ZONE
+    @region_counts = helpers.challenges_region_counts passed_checkpoint_ids
+    @sub_regions = @competition.regions.subs.order :name
+    @region_names = @sub_regions.pluck :name
+    @challenge_names = @competition.root_region.challenges.order(:name).pluck :name
   end
 end
