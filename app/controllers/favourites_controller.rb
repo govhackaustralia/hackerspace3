f2a5372cc1379953a2b04c10c9df1180acb91c76
Luke Cassar <luke.cassar@envisionapp.com>
2019-05-25 10:48:27 +1000

    Refactor favourites controller

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,24 +2,30 @@ class FavouritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @team = Team.find(params[:team_id])
     @favourite = Favourite.create(team: @team, assignment: current_user.event_assignment)
-    if @favourite.save
-      flash[:notice] = 'Favourite Added'
-    else
-      flash[:alert] = 'Error adding Favourite'
-    end
-    redirect_to project_path(@team.current_project.identifier)
+    team = Team.find params[:team_id]
+    handle_create favourite, team
   end
 
   def destroy
-    @favourite = Favourite.find_by(id: params[:id], assignment: current_user.event_assignment)
-    if @favourite.present?
-      @favourite.destroy
+    favourite = Favourite.find id: params[:id]
+    if favourite.present?
+      favourite.destroy
       flash[:notice] = 'Favourite Removed'
     else
       flash[:alert] = 'Problem removing favourite'
     end
-    redirect_to project_path(@favourite.team.current_project.identifier)
+    redirect_to project_path favourite.team.current_project.identifier
+  end
+
+  private
+
+  def handle_create(favourite, team)
+    if favourite.save
+      flash[:notice] = 'Favourite Added'
+    else
+      flash[:alert] = 'Error adding Favourite'
+    end
+    redirect_to project_path team.current_project.identifier
   end
 end
