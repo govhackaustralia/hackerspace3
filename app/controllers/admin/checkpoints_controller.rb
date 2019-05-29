732701ed4321b10fbc8418debe983f3aa9e1d9b9
Luke Cassar <luke.cassar@envisionapp.com>
2019-05-29 21:44:58 +1000

    Refactor admin checkpoints controller

--- app/controllers/admin/checkpoints_controller.rb
+++ app/controllers/admin/checkpoints_controller.rb
@@ -1,22 +1,19 @@
 class Admin::CheckpointsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :check_for_privileges
+  before_action :authenticate_user!, :check_for_privileges
 
   def index
-    @competition = Competition.find(params[:competition_id])
     @checkpoints = @competition.checkpoints
   end
 
   def new
-    @competition = Competition.find(params[:competition_id])
     @checkpoint = @competition.checkpoints.new
   end
 
   def create
-    create_new_checkpoint
+    @checkpoint = @competition.checkpoints.new checkpoint_params
     if @checkpoint.save
       flash[:notice] = 'Checkpoint created.'
-      redirect_to admin_competition_checkpoints_path(@competition)
+      redirect_to admin_competition_checkpoints_path @competition
     else
       flash[:alert] = @checkpoint.errors.full_messages.to_sentence
       render :new
@@ -24,15 +21,14 @@ class Admin::CheckpointsController < ApplicationController
   end
 
   def edit
-    @competition = Competition.find(params[:competition_id])
-    @checkpoint = Checkpoint.find(params[:id])
+    @checkpoint = Checkpoint.find params[:id]
   end
 
   def update
-    retrieve_competition_and_checkpoint
+    @checkpoint = Checkpoint.find params[:id]
     if @checkpoint.update(checkpoint_params)
       flash[:notice] = 'Checkpoint updated.'
-      redirect_to admin_competition_checkpoints_path(@competition)
+      redirect_to admin_competition_checkpoints_path @competition
     else
       flash[:alert] = @checkpoint.errors.full_messages.to_sentence
       render :edit
@@ -42,6 +38,7 @@ class Admin::CheckpointsController < ApplicationController
   private
 
   def check_for_privileges
+    @competition = Competition.find params[:competition_id]
     return if current_user.admin_privileges?
 
     flash[:alert] = 'You must have valid assignments to access this section.'
@@ -49,18 +46,11 @@ class Admin::CheckpointsController < ApplicationController
   end
 
   def checkpoint_params
-    params.require(:checkpoint).permit(:end_time, :name,
-                                       :max_national_challenges,
-                                       :max_regional_challenges)
-  end
-
-  def retrieve_competition_and_checkpoint
-    @competition = Competition.find(params[:competition_id])
-    @checkpoint = Checkpoint.find(params[:id])
-  end
-
-  def create_new_checkpoint
-    @competition = Competition.find(params[:competition_id])
-    @checkpoint = @competition.checkpoints.new(checkpoint_params)
+    params.require(:checkpoint).permit(
+      :end_time,
+      :name,
+      :max_national_challenges,
+      :max_regional_challenges
+    )
   end
 end
