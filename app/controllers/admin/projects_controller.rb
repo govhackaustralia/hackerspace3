33e9f4f5f1b65757a4a617ec1d6f7212137291d1
Luke Cassar <luke.cassar@envisionapp.com>
2019-05-30 21:56:40 +1000

    Refactor admin projects controller

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -3,13 +3,13 @@ class Admin::ProjectsController < ApplicationController
   before_action :check_for_privileges
 
   def index
-    @team = Team.find(params[:team_id])
+    @team = Team.find params[:team_id]
     @current_project = @team.current_project
-    @projects = @team.projects.order(created_at: :desc)
+    @projects = @team.projects.order created_at: :desc
   end
 
   def show
-    @project = Project.find(params[:id])
+    @project = Project.find params[:id]
     @user = @project.user
     @team = @project.team
     @current_project = @team.current_project
