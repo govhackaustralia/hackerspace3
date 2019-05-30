58de4495e1f4922e07a35e28c64e5587a8f8358e
Luke Cassar <luke.cassar@envisionapp.com>
2019-05-30 21:45:52 +1000

    Refactor admin event partnerships controller

--- app/controllers/admin/event_partnerships_controller.rb
+++ app/controllers/admin/event_partnerships_controller.rb
@@ -1,20 +1,20 @@
 class Admin::EventPartnershipsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :check_for_privileges
+  before_action :authenticate_user!, :check_for_privileges
 
   def new
-    new_event_partnership
+    @event_partnership = EventPartnership.new
     return if params[:term].blank?
 
-    @sponsor = Sponsor.find_by_name(params[:term])
+    @sponsor = Sponsor.find_by_name params[:term]
     @sponsors = Sponsor.search(params[:term]) unless @sponsor.present?
   end
 
   def create
-    create_new_event_partnership
+    @event_partnership = EventPartnership.new event_partnership_params
+    @event_partnership.event = @event
     if @event_partnership.save
       flash[:notice] = 'New Event Partner Added.'
-      redirect_to admin_region_event_path(@event.region, @event)
+      redirect_to admin_region_event_path @event.region, @event
     else
       flash.now[:alert] = @assignment.errors.full_messages.to_sentence
       render :new
@@ -22,34 +22,24 @@ class Admin::EventPartnershipsController < ApplicationController
   end
 
   def destroy
-    @event_partnership = EventPartnership.find(params[:id])
+    @event_partnership = EventPartnership.find params[:id]
     @sponsor = @event_partnership.sponsor
     @event_partnership.destroy
     flash[:notice] = 'Event Partnership Destroyed'
-    redirect_to admin_sponsor_path(@sponsor)
+    redirect_to admin_sponsor_path @sponsor
   end
 
   private
 
   def event_partnership_params
-    params.require(:event_partnership).permit(:sponsor_id)
+    params.require(:event_partnership).permit :sponsor_id
   end
 
   def check_for_privileges
     return if current_user.admin_privileges?
+    @event = Event.find params[:event_id]
 
     flash[:alert] = 'You must have valid assignments to access this section.'
     redirect_to root_path
-  end
-
-  def new_event_partnership
-    @event = Event.find(params[:event_id])
-    @event_partnership = EventPartnership.new
-  end
-
-  def create_new_event_partnership
-    @event = Event.find(params[:event_id])
-    @event_partnership = EventPartnership.new(event_partnership_params)
-    @event_partnership.event = @event
   end
 end
