f96f2affccc7099af9fc687bd459e4170da30b61
Luke Cassar <luke.cassar@envisionapp.com>
2019-06-02 10:50:37 +1000

    Refactor Sponsor model

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -11,12 +11,14 @@ class Sponsor < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, presence: true
 
-  scope :search, ->(term) { where 'name ILIKE ? OR description ILIKE ?', "%#{term}%", "%#{term}%" }
+  scope :search, lambda { |term|
+    where 'name ILIKE ? OR description ILIKE ?', "%#{term}%", "%#{term}%"
+  }
 
   # Returns true if a user is able to see a sponsor's settings.
   # ENHANCEMENT: Move to Controller.
   def show_privileges?(user)
-    (user.assignments.pluck(:title) & [SPONSOR_CONTACT]).present?
+    user.assignments.where(title: SPONSOR_CONTACT).any?
   end
 
   # Returns all the assignments able to modify and interact with a sponsor.
