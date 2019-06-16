feeda9028693148cc92f5544cae936a75945679d
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-06-16 19:46:40 +1000

    Refactor entries index

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,6 +4,7 @@ class EntriesController < ApplicationController
     @region_counts = helpers.challenges_region_counts @competition, passed_checkpoint_ids
     @sub_regions = @competition.regions.subs.order :name
     @region_names = @sub_regions.pluck :name
-    @challenge_names = @competition.root_region.challenges.order(:name).pluck :name
+    @root_region = @competition.root_region
+    @challenge_names = @root_region.challenges.order(:name).pluck :name
   end
 end
