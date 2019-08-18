b37555d775fb2cbe7cb36a8707dd58450ac4d721
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-08-18 11:00:30 +1000

    Restyle team controller test

--- test/controllers/admin/teams_controller_test.rb
+++ test/controllers/admin/teams_controller_test.rb
@@ -19,14 +19,18 @@ class Admin::TeamsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should patch update project' do
-    patch admin_competition_team_url @competition, @team, params: { project_id: @project.id }
+    patch admin_competition_team_url @competition, @team, params: {
+      project_id: @project.id
+    }
     assert_redirected_to admin_team_project_url @team, @project
     @team.reload
     assert @team.current_project == @project
   end
 
   test 'should patch update published' do
-    patch admin_competition_team_url @competition, @team, params: { published: false }
+    patch admin_competition_team_url @competition, @team, params: {
+      published: false
+    }
     assert_redirected_to admin_competition_team_url @competition, @team
     @team.reload
     assert_not @team.published
