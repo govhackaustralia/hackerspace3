022f2986ad3b9adfb7904e6594a8ee2b2020dec1
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-09-05 14:50:34 +1000

    Remove privilige helper methods from competition helper

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -1,19 +1,2 @@
 module CompetitionsHelper
-  # Returns true if the competition has started or if a user has region
-  # privileges
-  def competition_started_or_region_privileges?(time_zone = nil)
-    @competition.started?(time_zone) || @region_privileges
-  end
-
-  # Return true if the cometition is in the team form stage or has started or
-  # if a user as region privileges
-  def in_form_or_comp_started_or_region_privileges?(time_zone = nil)
-    @competition.in_form_or_comp_started?(time_zone) || @region_privileges
-  end
-
-  # Returns true if a competition window is open or a user has region
-  # privileges
-  def in_form_or_comp_window_or_region_privileges?(time_zone = nil)
-    @competition.in_form_or_comp_window?(time_zone) || @region_privileges
-  end
 end
--- test/helpers/competitions_helper_test.rb
+++ test/helpers/competitions_helper_test.rb
@@ -1,20 +1,4 @@
 require 'test_helper'
 
 class CompetitionsHelperTest < ActionView::TestCase
-  setup do
-    @competition = Competition.first
-    @region_privileges = true
-  end
-
-  test 'competition_started_or_region_privileges?' do
-    assert competition_started_or_region_privileges?
-  end
-
-  test 'in_form_or_comp_started_or_region_privileges?' do
-    assert in_form_or_comp_started_or_region_privileges?
-  end
-
-  test 'in_form_or_comp_window_or_region_privileges?' do
-    assert in_form_or_comp_window_or_region_privileges?
-  end
 end
