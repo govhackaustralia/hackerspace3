e08307b18cd153c22484cb0fb9f0ecac8b18e280
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-09-05 23:51:01 +1000

    Challenges to be displayed at opening of comp in NZ

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -46,6 +46,7 @@ MAX_DECISION_TIME = 2.minutes
 
 CODE_OF_CONDUCT_URL = 'https://govhack.org/competition/code-of-conduct/'
 
+FIRST_TIME_ZONE = 'Wellington'
 LAST_TIME_ZONE = 'Perth'
 COMP_TIME_ZONE = 'Sydney'
 
