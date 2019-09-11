b399839846966bb9a8b22f17bac8990562ca6621
Luke Cassar <luke.cassar@uqconnect.edu.au>
2019-09-11 12:25:11 +1000

    Move check for privileges to before_action in admin projects controller

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,11 +1,9 @@
 class Admin::ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :check_for_privileges!
 
   def index
-    @team = Team.find params[:team_id]
     @current_project = @team.current_project
     @projects = @team.projects.order created_at: :desc
-    check_for_privileges @team.competition
   end
 
   def show
@@ -13,13 +11,14 @@ class Admin::ProjectsController < ApplicationController
     @user = @project.user
     @team = @project.team
     @current_project = @team.current_project
-    check_for_privileges @team.competition
   end
 
   private
 
-  def check_for_privileges(competition)
-    return if current_user.admin_privileges? competition
+  def check_for_privileges!
+    @team = Team.find params[:team_id]
+    @competition = @team.competition
+    return if current_user.admin_privileges? @competition
 
     flash[:alert] = 'You must have valid assignments to access this section.'
     redirect_to root_path
