cea17ee48856eda60cc1d95be47e7221b83700ae
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-01-16 07:41:53 +1000

    Add unaccounted team data set count method to sponsor data set report

--- app/services/sponsor_data_set_report.rb
+++ app/services/sponsor_data_set_report.rb
@@ -1,14 +1,23 @@
 class SponsorDataSetReport
-  attr_reader :competition, :data_sets
   require 'csv'
+
+  attr_reader :competition, :data_sets, :accounted_team_data_sets
 
   def initialize(competition)
     @competition = competition
     @data_sets = competition.data_sets.preload(:challenges, :sponsors)
+    @accounted_team_data_sets = nil
   end
 
   def report
+    @accounted_team_data_sets = []
     data_sets.collect { |data_set| data_set_result(data_set) }
+  end
+
+  def unaccounted_team_data_set_count
+    raise 'Run #report first!' if accounted_team_data_sets.nil?
+
+    (competition.team_data_sets.pluck(:id) - accounted_team_data_sets).length
   end
 
   def to_csv
@@ -28,6 +37,11 @@ class SponsorDataSetReport
 
   def data_set_result(data_set)
     team_data_sets = competition.team_data_sets.where(url: data_set.url)
+    accounted_team_data_sets << team_data_sets.pluck(:id)
+    data_set_object(data_set, team_data_sets)
+  end
+
+  def data_set_object(data_set, team_data_sets)
     {
       data_set_name: data_set.name,
       challenges: data_set.challenges.pluck(:name),
--- test/services/sponsor_data_set_report_test.rb
+++ test/services/sponsor_data_set_report_test.rb
@@ -10,6 +10,15 @@ class SponsorDataSetReportTest < ActiveSupport::TestCase
     assert @sponsor_dataset_report.report.class == Array
   end
 
+  test 'unaccounted_team_data_set_count' do
+    assert_raises(StandardError) do
+      @sponsor_dataset_report.unaccounted_team_data_set_count
+    end
+
+    @sponsor_dataset_report.report
+    assert @sponsor_dataset_report.unaccounted_team_data_set_count.class == Integer
+  end
+
   test 'to_csv' do
     assert @sponsor_dataset_report.to_csv.class == String
   end
