c0b8e07cab300088151647316b90310fc1994ebd
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-03-21 10:43:46 +1000

    Comment out content policy connect scr as I don't know what it does

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -27,4 +27,4 @@
 # You need to allow webpack-dev-server host as allowed origin for connect-src.
 # This can be done in Rails 5.2+ for development environment in the CSP initializer
 # config/initializers/content_security_policy.rb with a snippet like this:
-policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+# policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
