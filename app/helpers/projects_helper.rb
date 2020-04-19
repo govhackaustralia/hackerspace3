d01019646dd468ee7b24d942636980674194d987
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-04-19 19:06:05 +1000

    Remove filter projects helper as no longer needed

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,23 +1,2 @@
 module ProjectsHelper
-  def filter_projects(term)
-    return @projects if term.nil?
-
-    filtered_projects = []
-    @projects.each do |project|
-      next unless search_match(project, term)
-
-      filtered_projects << project
-    end
-    filtered_projects
-  end
-
-  private
-
-  def search_match(project, term)
-    team_name = project.team_name
-    project_name = project.project_name
-    event_name = project.event.name
-    team_string = "#{team_name} #{project_name} #{event_name}".downcase
-    team_string.include?(term.downcase) ? true : false
-  end
 end
