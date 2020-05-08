cdf99607cdb95564125f4d0e734682e7ce08917b
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-05-09 08:20:51 +1000

    Updates from rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_010825) do
+ActiveRecord::Schema.define(version: 2020_05_08_043709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -467,6 +467,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_010825) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "holders", "competitions"
   add_foreign_key "holders", "users"
 end
