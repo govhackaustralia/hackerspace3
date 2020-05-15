8c26ba702102077fee1f9f951fdc4418d2184bdf
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-05-16 07:51:02 +1000

    Add acts-as-taggable gem and migrations

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem 'bundler-audit'
 gem 'omniauth-rails_csrf_protection'
 # Use Webpack to manage app-like JavaScript modules in Rails
 gem 'webpacker'
+# A tagging plugin that allows for custom tagging along dynamic contexts.
+gem 'acts-as-taggable-on', '~> 6.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_043709) do
+ActiveRecord::Schema.define(version: 2020_05_15_214822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -369,6 +369,33 @@ ActiveRecord::Schema.define(version: 2020_05_08_043709) do
     t.index ["sponsorable_type", "sponsorable_id"], name: "index_sponsorships_on_sponsorable_type_and_sponsorable_id"
   end
 
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
+  end
+
   create_table "team_data_sets", force: :cascade do |t|
     t.integer "team_id"
     t.string "name"
@@ -470,4 +497,5 @@ ActiveRecord::Schema.define(version: 2020_05_08_043709) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "holders", "competitions"
   add_foreign_key "holders", "users"
+  add_foreign_key "taggings", "tags"
 end
