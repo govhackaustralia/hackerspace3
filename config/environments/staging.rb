5dad6bf3aa652d5cc6191fd1551ced4d08419421
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-05-18 11:54:06 +1000

    Add staging config for action cable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://#{ENV['DOMAIN']}/cable"
-  config.action_cable.allowed_request_origins = ["http://#{ENV['DOMAIN']}"]
+  config.action_cable.url = "ws://#{ENV['DOMAIN']}/cable"
+  config.action_cable.allowed_request_origins = ['https://staging.hackerspace.govhack.org', 'govhack.org']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
