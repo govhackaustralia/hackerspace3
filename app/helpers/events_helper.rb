01dab2c33bdfadc8362cffe9da27eb448d4d7e7a
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-06-27 15:07:20 +1000

    Refactor participant_able_to_enter? helper method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,7 @@
 module EventsHelper
   def participant_able_to_enter?
-    !(@event.competition_event? &&
-      @competition.already_participating_in_a_competition_event?(@event_assignment))
+    return true unless @event.competition_event?
+
+    ! @competition.already_participating_in_a_competition_event?(@event_assignment)
   end
 end
