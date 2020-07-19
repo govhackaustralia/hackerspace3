a96637af219be4645a6a8bf548bed6ebbf8e6f7c
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-07-19 23:04:44 +1000

    Add connections events to the bottom of the conference page

--- app/controllers/conference_controller.rb
+++ app/controllers/conference_controller.rb
@@ -4,5 +4,7 @@ class ConferenceController < ApplicationController
       .order start_time: :asc, name: :asc
     @future_sessions = all.conferences.future
     @past_sessions = all.conferences.past
+    @past_connections = all.connections.past
+    @future_connections = all.connections.future
   end
 end
