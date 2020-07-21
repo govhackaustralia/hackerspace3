02bd9b99d5f55462642649f4098f4d22a00fecb5
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-07-21 22:42:19 +1000

    Implement public facing badge and profile screens

---
+++ test/controllers/profile_pictures_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ProfilePicturesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    sign_in users :one
+  end
+
+  test 'should get edit' do
+    get update_profile_picture_path
+    assert_response :success
+  end
+
+  test 'sholud patch update' do
+    # Test This
+  end
+end
