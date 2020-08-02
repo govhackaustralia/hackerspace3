953ad0da9cc22730b07d2da3e423e38a7354b87a
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-08-02 21:16:47 +1000

    Add profile edit form and extra fields

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only: %i[edit update]
+  before_action :authorize_user!, only: %i[edit update]
+
   def index
     @profiles = Profile.all.where.not(identifier: nil).preload(:user).includes(:skills)
   end
@@ -7,5 +10,28 @@ class ProfilesController < ApplicationController
     @profile = Profile.find_by_identifier params[:id]
     @user = @profile.user
     @badges = @user.badges
+    @team_name = @user.joined_teams.first&.current_project&.team_name
+  end
+
+  def edit; end
+
+  def update
+    @profile.update profile_params
+    redirect_to profile_path(@profile), notice: 'Your Profile has been updated'
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:team_status, :description, :website,
+      :twitter, :linkedin, :skill_list, :interest_list)
+  end
+
+  def authorize_user!
+    @profile = Profile.find_by_identifier params[:id]
+    return if current_user.profile == @profile
+
+    redirect_to profile_path(@profile),
+      alert: 'You are not permitted to edit this Profile'
   end
 end
