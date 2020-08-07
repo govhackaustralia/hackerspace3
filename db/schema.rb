64065adc002e02ca43e0ef95df98002ec3aeae2d
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-08-08 00:21:27 +1000

    Add github attribute to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_041348) do
+ActiveRecord::Schema.define(version: 2020_08_07_141225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -293,6 +293,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_041348) do
     t.string "linkedin"
     t.string "twitter"
     t.string "description"
+    t.string "github"
     t.index ["identifier"], name: "index_profiles_on_identifier"
   end
 
