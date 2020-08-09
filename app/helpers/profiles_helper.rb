378f094b772c910f9b93ef1952bd491b4412591e
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-08-09 19:46:54 +1000

    Check for profile before querying it

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,7 +1,7 @@
 module ProfilesHelper
   def default_profile_picture_url(user, profile)
-    if profile.profile_picture.attached?
-      url_for profile&.profile_picture
+    if profile.present? && profile.profile_picture.attached?
+      url_for profile.profile_picture
     elsif user.google_img.present?
       user.google_img
     else
