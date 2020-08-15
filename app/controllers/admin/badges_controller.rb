7d7b96c4dd57a87df359e65d0744b6c25e94b381
Luke Cassar <luke.cassar@uqconnect.edu.au>
2020-08-15 21:01:31 +1000

    Try and scope users even further in badge controller

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -8,6 +8,7 @@ class Admin::BadgesController < ApplicationController
   def show
     @badge = @competition.badges.find_by_identifier params[:id]
     @users = User.joins(:profile)
+      .where.not(full_name: [nil, ''])
       .where.not(profiles: {identifier: [nil, '']})
       .order(:email)
       .preload(:profile, :badge_assignments)
