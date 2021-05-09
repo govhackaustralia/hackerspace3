74c080161050ef3980e26e71bc5b8748da807ddc
Luke Cassar <luke.cassar@uqconnect.edu.au>
2021-05-09 11:26:53 +1000

    Revert "Add Team Datasets"

--- app/models/team_dataset.rb
+++
@@ -1,9 +0,0 @@
-class TeamDataset < ApplicationRecord
-  belongs_to :dataset
-  belongs_to :team
-
-  validates :team_id, uniqueness: {
-    scope: :dataset_id,
-    message: 'Team Dataset already exists'
-  }
-end
