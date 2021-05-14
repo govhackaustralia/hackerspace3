42965e8cbadc3a564299a4890c22176cc75f5618
Luke Cassar <luke.cassar@uqconnect.edu.au>
2021-05-15 09:34:56 +1000

    Revert "Revert "Move Invisible Captcha to Registration Form""

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: :create
+  invisible_captcha honeypot: :terms_and_conditions, only: :create
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:terms_and_conditions])
+  end
+end
