c250a84268f0da4f1bbb6a752cdfcde2cbd24679
Luke Cassar <development@govhack.org>
2021-06-16 20:53:38 +1000

    Replace DEFAULT_FROM_EMAIL with a default value

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['DEFAULT_FROM_EMAIL']
+  default from: ENV['DEFAULT_FROM_EMAIL'] || 'notifications@hackerspace.com'
   helper(ApplicationHelper)
   layout 'mailer'
 end
