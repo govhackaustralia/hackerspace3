3c84899441a48e2211de8f602698e3e2b3b85c52
Luke Cassar <development@govhack.org>
2021-06-16 21:21:08 +1000

    Remove old mailchimp file

--- mailchimp.rb
+++ mailchimp.rb
@@ -1,24 +1,2 @@
-require 'MailchimpMarketing'
-
-# Run on command line with $ rails runner mailchimp.rb
-
-begin
-  client = MailchimpMarketing::Client.new
-  client.set_config({ api_key: ENV['MAILCHIMP_API_KEY'], server: ENV['MAILCHIMP_SERVER_PREFIX'] })
-
-  response =
-    client.lists.add_list_member(
-      ENV['MAILCHIMP_LIST_ID'],
-      {
-        'email_address' => 'Kelley.Haley28@hotmail.com',
-        'status' => 'subscribed',
-        'merge_fields' => {
-          'FNAME' => 'Kelley',
-          'LNAME' => 'Haley'
-        }
-      }
-    )
-  p response
-rescue MailchimpMarketing::ApiError => e
-  puts "Error: #{e}"
-end
+gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'], symbolize_keys: true)
+gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: "luke.cassar@hey.com", status: "subscribed", merge_fields: {FNAME: "Luke", LNAME: "Cassar Hey"}})
