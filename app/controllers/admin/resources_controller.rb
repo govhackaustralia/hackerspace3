03cd78144604f294f9a9cd5c4ec819c356993385
Luke Cassar <development@govhack.org>
2021-07-26 22:21:31 +1000

    Fix resource controller so it pulls correct competition

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -48,6 +48,10 @@ class Admin::ResourcesController < ApplicationController
     params.require(:resource).permit(:name, :position, :category, :url, :short_url)
   end
 
+  def competition
+    @competition = Competition.find params[:competition_id]
+  end
+
   def authorise_user!
     return if current_user.admin_privileges? @competition
 
