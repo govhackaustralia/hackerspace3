04eadef8de079f907e2cf8b36782566d23aed818
Luke Cassar <development@govhack.org>
2021-08-16 22:41:09 +1000

    Add check for existing team name before requesting update from slack

--- app/jobs/update_channel_name_job.rb
+++ app/jobs/update_channel_name_job.rb
@@ -9,9 +9,28 @@ class UpdateChannelNameJob < ApplicationJob
 
   def rename_slack_channel(project)
     team = project.team
-    return if team.nil? || team.slack_channel_id.nil?
+    return unless can_update_channel?(team)
 
-    SlackApiWrapper.slack_conversatons_rename(
-      team.slack_channel_id, project.slack_channel_name)
+    return unless should_update_channel?(project, team)
+
+    response = SlackApiWrapper.slack_conversatons_rename(
+      channel_id: team.slack_channel_id,
+      channel_name: project.slack_channel_name
+    )
+    raise response['error'] unless response['ok']
+    update_team_channel_name(team, response)
+  end
+
+  def update_team_channel_name(team, response)
+    new_channel_name = response.dig('channel', 'name')
+    team.update! slack_channel_name: new_channel_name
+  end
+
+  def can_update_channel?(team)
+    team.present? && team.slack_channel_id.present?
+  end
+
+  def should_update_channel?(project, team)
+    project.slack_channel_name != team.slack_channel_name
   end
 end
