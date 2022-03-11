64be8f3fa6b65e4bb3ba324991699347ac9db744
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-03-11 14:08:02 +1100

    Remove unused constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,46 +1,8 @@
-# E-mail Regex
-# Source: Michael Hartl Ruby on Rails Tutorial
-# https://www.railstutorial.org/book/modeling_users
-
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-# General String Lengths
-# Source: Old twitter tweet limit.
-# Note: Increase as needed.
-
-MAX_STRING_LENGTH = 140
-
-# Twitter Handles
-# Source: Help with Username Registration
-# https://help.twitter.com/en/managing-your-account/twitter-username-rules
-
-MAX_TWITTER_LENGTH = 15
-
 # Competition Judgement Scores
 # Source: Project Specification
 
 MIN_SCORE = 1
 MAX_SCORE = 10
-
-# URL Limits
-# Source: Stack Overflow
-# https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers
-
-MAX_URL_LENGTH = 2000
-
-# Other Field Lengths
-# Source: From the Ether :)
-# Note: Increase as needed.
-
-MAX_TEXT_LENGTH = 5000
-MAX_LOCATION_NAME_LENGTH = 50
-MAX_ATTENDANCE_CAPACITY = 200,000
-MAX_FULL_NAME_LENGTH = 1000
-PHONE_NUMBER_LENGTH = 10
-
-# Maximum time attendee has before place is potentially lost to someone else.
-
-MAX_DECISION_TIME = 2.minutes
 
 # Govhack Code of Conduct Page
 
