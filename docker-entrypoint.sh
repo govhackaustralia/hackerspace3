1a630a8b4bab2432e6e7eefee01255c8bb168235
Jaffar Rizvi <J.Rizvi@outlook.com>
2022-06-23 22:11:34 +1000

    Terminate pid from container before running server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ -f /usr/src/app/tmp/pids/server.pid ]; then
+  rm /usr/src/app/tmp/pids/server.pid
+fi
+
 # Initialise Rails database if it doesn't already exist
 if ! rails db:version >/dev/null 2>&1
 then
