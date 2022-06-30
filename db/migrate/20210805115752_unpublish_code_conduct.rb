a2e90760491a82b2a0d89519998b50782264cdbf
Luke Cassar <luke.cassar@uqconnect.edu.au>
2022-06-30 22:37:46 +1000

    Remove 2021 migrations

--- db/migrate/20210805115752_unpublish_code_conduct.rb
+++
@@ -1,7 +0,0 @@
-class UnpublishCodeConduct < ActiveRecord::Migration[6.1]
-  def change
-    User.joins(:profile).where(accepted_code_of_conduct: nil, profiles: {published: true}).preload(:profile).each do |user|
-      user.profile.update! published: false
-    end
-  end
-end
--- db/migrate/20210814115008_slack_team_name.rb
+++
@@ -1,5 +0,0 @@
-class SlackTeamName < ActiveRecord::Migration[6.1]
-  def change
-    add_column :teams, :slack_channel_name, :string
-  end
-end
