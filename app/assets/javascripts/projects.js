42e770a99a8ff29e9189f75fd8484ff148cde1e5
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-05-23 16:47:02 +0930

    Update projects.js

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,16 +2,12 @@
 // All this logic will automatically be available in application.js.
 $( document ).on('ready turbolinks:load', function() {
 
-  var windowObjectReference;
-  var strWindowFeatures = "menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=500,height=800";
+  const strWindowFeatures = "menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=500,height=800";
 
-  function openRequestedPopup() {
-    windowObjectReference = window.open(_scorecard_path, 'Scorecard', strWindowFeatures);
-  }
+  const openRequestedPopup = () => windowObjectReference = window.open(_scorecard_path, 'Scorecard', strWindowFeatures);
+  
 
-  function refreshParent() {
-    window.opener.location.reload();
-  }
+  const refreshParent = () => window.opener.location.reload();
 
   $('#open-scorecard').click(function (event) {
     event.preventDefault();
