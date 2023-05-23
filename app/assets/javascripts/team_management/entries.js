e71fb3002738794f669545a7cf913469f588767b
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-05-23 16:51:36 +0930

    Update entries.js
    we did not need to separate the URL variable into a separate "separated" variable so i changed it to one variable to get and split the variable.
    
    also replaced some of the vars with const

--- app/assets/javascripts/team_management/entries.js
+++ app/assets/javascripts/team_management/entries.js
@@ -3,22 +3,22 @@
 
 $( document ).on('ready turbolinks:load', function() {
 	$("#team_data_set_url").blur(function() {
-		var url = $("#team_data_set_url").val();
-		var separated = url.split("/dataset/");
-		console.log(url);
+		const dataset = $("#team_data_set_url").val().split("/dataset/");
+		
 		var data = {
-			id: separated[1]
+			id: dataset[1]
 		}
 		$.ajax({
 			type: "GET",
 			data: data,
-			url: separated[0] + "/api/3/action/package_show",
+			url: dataset[0] + "/api/3/action/package_show",
 			success: function (data) {
 				$("#team_data_set_name").val(data.result.title);
 				$("#team_data_set_description").val(data.result.notes);
-				
 				$("#hiddendescription").width($("#data_set_description").width());
-				var content = data.result.notes.replace(/\n/g, "<br>");
+
+				const content = data.result.notes.replace(/\n/g, "<br>");
+
 				$("#hiddendescription").html(content);
 				$("#team_data_set_description").css("height", $("#hiddendescription").outerHeight());
 			},
@@ -27,7 +27,9 @@ $( document ).on('ready turbolinks:load', function() {
 	});
 	try {
 	$("#hiddendescription").width($("#data_set_description").width());
-	var content = $("#team_data_set_description").val().replace(/\n/g, "<br>");
+
+	const content = $("#team_data_set_description").val().replace(/\n/g, "<br>");
+
 	$("#hiddendescription").html(content);
 	$("#team_data_set_description").css("height", $("#hiddendescription").outerHeight());
 	} catch (e) {
