df33de72dcbf6404ad3098ac2038add72d5ed8a6
Munyaradzi Magura <munyaradziMagura@outlook.com>
2023-05-23 16:46:55 +0930

    Update scorecards.js

--- app/assets/javascripts/admin/teams/scorecards.js
+++ app/assets/javascripts/admin/teams/scorecards.js
@@ -3,16 +3,13 @@
 
 $( document ).on('ready turbolinks:load', function() {
 
-  var windowObjectReference;
   var strWindowFeatures = "menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=700,height=800";
 
-  function openRequestedPopup(team_scorecards_path) {
-    windowObjectReference = window.open(team_scorecards_path, 'Team Scorecards', strWindowFeatures);
-  }
+  const openRequestedPopup = (team_scorecards_path) => windowObjectReference = window.open(team_scorecards_path, 'Team Scorecards', strWindowFeatures);
 
-  function refreshParent() {
-    window.opener.location.reload();
-  }
+
+  const refreshParent = () =>  window.opener.location.reload();
+  
 
   $('.open-team-scorecards').click(function (event) {
     event.preventDefault();
