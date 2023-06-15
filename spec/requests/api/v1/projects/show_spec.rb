3bbdc58faaf791d8ec444157a1ed26d8f4284d74
Blake Astley <astley92@hotmail.com>
2023-06-15 20:24:52 +1000

    Render record not found errors correctly

--- spec/requests/api/v1/projects/show_spec.rb
+++ spec/requests/api/v1/projects/show_spec.rb
@@ -11,9 +11,10 @@ RSpec.describe '/api/v1/projects' do
       project_name: 'My awesome project',
     )
   end
+  let(:requested_id) { 1 }
 
   it 'the requested project' do
-    get '/api/v1/projects/1'
+    get "/api/v1/projects/#{requested_id}"
 
     expect(response).to have_http_status(:success)
     parsed_body = JSON.parse(response.body, symbolize_names: true)
@@ -24,5 +25,26 @@ RSpec.describe '/api/v1/projects' do
         project_name: 'My awesome project',
       },
     )
+  end
+
+  context "when the project doesn't exist" do
+    let(:requested_id) { 2 }
+
+    it 'returns a 404 with the expected structure' do
+      get "/api/v1/projects/#{requested_id}"
+
+      expect(response).to have_http_status(:not_found)
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body.fetch(:errors)).to eq(
+        [
+          {
+            status: '404',
+            title: 'Record not found',
+            detail: 'The record you requested could not be found',
+          },
+        ],
+      )
+    end
   end
 end
