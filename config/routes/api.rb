f532c97e08280bf22e3d288f5025d5ff76730002
Blake Astley <astley92@hotmail.com>
2023-07-06 20:56:39 +1000

    Add event index endpoint with pagination

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -2,6 +2,7 @@ namespace :api do
   namespace :v1 do
     get "/health", to: "health#show"
 
+    resources :events, only: %i[index]
     resources :projects, only: %i[index show]
   end
 end
---
+++ spec/serializers/event_blueprint_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe EventBlueprint do
+  subject { described_class.render_as_hash(event) }
+
+  let(:event) { create(:event, id: 1234, region: region) }
+  let(:region) { create(:region, competition: create(:competition)) }
+
+  it 'renders the event' do
+    expect(subject).to eq(
+      {
+        id: 1234,
+      },
+    )
+  end
+end
