402b480d0442afba289c0065c36557b3da9346fa
ArjunAranetaCodes <arjunaraneta@gmail.com>
2024-08-15 21:25:15 +0800

    Revert "Added changes for show and hide password"
    This reverts commit f69d16465f746d977eb2b861fe67db579b76c3cd.

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,16 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-function togglePasswordVisibility() {
-  var passwordField = document.getElementById("password-field");
-  var toggleIcon = document.getElementById("toggle-password-icon");
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    toggleIcon.classList.remove("fa-eye");
-    toggleIcon.classList.add("fa-eye-slash");
-  } else {
-    passwordField.type = "password";
-    toggleIcon.classList.remove("fa-eye-slash");
-    toggleIcon.classList.add("fa-eye");
-  }
-}
