06785072bc43cf493b0d83585ed616850ed54d9d
Arjun Araneta <arjunaraneta@gmail.com>
2024-09-20 03:01:16 +0800

    Bug/challenge table 2 (#466)
    * fix for challenge table
    
    * added fix for challenge csv download

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -91,12 +91,15 @@ class Challenge < ApplicationRecord
 
   # Compiles a CSV file of all challenges.
   # ENHANCEMENT: This should be in a helper object or own model.
-  def self.to_csv(competition, options = {})
+  def self.to_csv(competition, options={})
     challenge_columns = %w[id name short_desc long_desc eligibility video_url created_at updated_at]
-    CSV.generate(options) do |csv|
+    #CSV.generate(options) do |csv|
+    CSV.generate do |csv|
       csv << (%w[region_name competition_year] + challenge_columns + %w[sponsors entries])
       competition.challenges.preload(:region, :sponsors, :published_entries).each do |challenge|
-        csv << challenge_csv_line(challenge, competition, challenge_columns)
+        formatted_values = challenge_csv_line(challenge, competition, challenge_columns).map(&:to_s)
+        csv << formatted_values
+        #csv << challenge_csv_line(challenge, competition, challenge_columns)
       end
     end
   end
