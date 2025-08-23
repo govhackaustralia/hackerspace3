652b809ffb334b51a37edac8ec9de0f4998ef5fb
cenon-delrosario <37419322+cenon-delrosario@users.noreply.github.com>
2025-08-23 15:17:15 +1000

    Add deactivated_at to User model to allow users to be deactivated (#476)
    Changes:
    
    * DB migration
    * Schema and Model updates
    * Change Profile controller to filter out deactivated Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_130633) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_22_200000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -523,6 +523,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_11_130633) do
     t.boolean "under_18"
     t.integer "region"
     t.integer "acting_on_behalf_of_id"
+    t.datetime "deactivated_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["region"], name: "index_users_on_region"
